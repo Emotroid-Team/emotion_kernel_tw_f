ca1644733971c7f34c91819cf3fc3b46fc3eda20
Amir Samuelov <amirs@codeaurora.org>
2014-07-06 13:51:12 +0300

    platform: msm: fix PFT for 64-bit
    Fix compile warning for 64-bit platform.
    
    Signed-off-by: Amir Samuelov <amirs@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: placiano <placiano80@gmail.com>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -24,8 +24,8 @@
 #include <linux/binfmts.h>
 #include <linux/in.h>
 #include <linux/spinlock.h>
-#include "flask.h"
-#include "avc.h"
+#include <net/net_namespace.h>
+#include <linux/in6.h>
 
 struct task_security_struct {
 	u32 osid;		/* SID prior to last execve */
