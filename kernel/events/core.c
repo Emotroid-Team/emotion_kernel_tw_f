1b3e4c03b5974565f0886c43e1327f4e359f504f
imoseyon <imoseyon@gmail.com>
2014-11-23 10:15:55 -0800

    perf: fix section mismatch warning
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1354,7 +1354,7 @@ static int __perf_remove_from_context(void *info)
 }
 
 #ifdef CONFIG_SMP
-static void perf_retry_remove(struct remove_event *re)
+static void __ref perf_retry_remove(struct remove_event *re)
 {
 	int up_ret;
 	/*
