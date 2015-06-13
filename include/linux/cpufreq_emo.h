54dfe00417743c520992a64f5e64baf7b1300b19
placiano <placiano80@gmail.com>
2015-06-13 23:14:49 +0200

    Hardlimit adapted from Boeffla S5 kernel. *Added freq_lock by Ktoonsez *Fixed policy to works with cpu1,cpu2 and cpu3
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>
    Signed-off-by: placiano <placiano80@gmail.com>

---
+++ include/linux/cpufreq_emo.h
@@ -0,0 +1,5 @@
+#include <linux/cpufreq.h>
+#include <linux/cpu.h>
+
+extern unsigned int isBooted;
+extern unsigned int vfreq_lock;
--- kernel/power/main.c
+++ kernel/power/main.c
@@ -25,6 +25,7 @@
 
 #ifdef CONFIG_CPU_FREQ_LIMIT_USERSPACE
 #include <linux/cpufreq.h>
+#include <linux/cpufreq_emo.h>
 #include <linux/cpufreq_limit.h>
 #endif
 
@@ -784,6 +785,9 @@ static ssize_t cpufreq_min_limit_store(struct kobject *kobj,
 		goto out;
 	}
 
+	if ((vfreq_lock || !isBooted) && val != -1)
+		return n;
+
 	mutex_lock(&cpufreq_limit_mutex);
 	if (cpufreq_min_hd) {
 		cpufreq_limit_put(cpufreq_min_hd);
@@ -817,6 +821,9 @@ struct cpufreq_limit_handle *cpufreq_min_touch;
 int set_freq_limit(unsigned long id, unsigned int freq)
 {
 	ssize_t ret = -EINVAL;
+
+	if ((vfreq_lock || !isBooted) && freq != -1)
+		return 0;
 
 	mutex_lock(&cpufreq_limit_mutex);
 
