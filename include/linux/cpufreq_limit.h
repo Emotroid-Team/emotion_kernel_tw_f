3b15a18668882c4084607a9bce4f1dba13d4e1ea
placiano <placiano80@gmail.com>
2015-06-17 01:19:31 +0200

    Hardlimit: moderate boost to frequency defaults
    This reverts commit cb5d8c22bca1ee35d79e37ccb2f4b1d09d57627b.
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- include/linux/cpufreq_limit.h
+++ include/linux/cpufreq_limit.h
@@ -17,7 +17,7 @@ struct cpufreq_limit_handle;
 #define MAX_FREQ_LIMIT	2649600
 #define MIN_FREQ_LIMIT	268000
 #ifdef CONFIG_SEC_PM
-#define SUSPEND_BOOST	1190400
+#define SUSPEND_BOOST	1497600
 #endif
 
 struct cpufreq_limit_handle *cpufreq_limit_get(unsigned long min_freq,
